e682c52ba722da988d0af3d6f25312250759106b
KenHoang16CDTH12 <angelkittybihoang@gmail.com>
2017-04-22 16:48:29 +0700

    Model Action

---
+++ app/src/main/java/app/hueic/hueicprojectdoanthanhnien/model/Action.java
@@ -0,0 +1,24 @@
+package app.hueic.hueicprojectdoanthanhnien.model;
+
+/**
+ * Created by kenhoang on 4/22/17.
+ */
+
+public class Action {
+    private String nameAction;
+
+    public String getNameAction() {
+        return nameAction;
+    }
+
+    public void setNameAction(String nameAction) {
+        this.nameAction = nameAction;
+    }
+
+    public Action(String nameAction) {
+        this.nameAction = nameAction;
+    }
+
+    public Action() {
+    }
+}
