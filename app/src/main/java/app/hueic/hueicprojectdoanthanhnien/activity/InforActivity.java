e5aa71f0db94f18523f681cb545156de207fa5c3
KenHoang16CDTH12 <angelkittybihoang@gmail.com>
2017-04-22 17:54:50 +0700

    new Activity ==> InforActivity.java & activity_infor.xml

---
+++ app/src/main/java/app/hueic/hueicprojectdoanthanhnien/activity/InforActivity.java
@@ -0,0 +1,15 @@
+package app.hueic.hueicprojectdoanthanhnien.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.hueic.hueicprojectdoanthanhnien.R;
+
+public class InforActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infor);
+    }
+}
