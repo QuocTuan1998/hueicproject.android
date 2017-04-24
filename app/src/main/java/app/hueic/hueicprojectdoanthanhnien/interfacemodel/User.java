91d487fd0a4949981986557a66cd167a9cd4703b
KenHoang16CDTH12 <angelkittybihoang@gmail.com>
2017-04-24 21:51:55 +0700

    create interface User (username, password,  idUser, role)

---
+++ app/src/main/java/app/hueic/hueicprojectdoanthanhnien/interfacemodel/User.java
@@ -0,0 +1,14 @@
+package app.hueic.hueicprojectdoanthanhnien.interfacemodel;
+
+import app.hueic.hueicprojectdoanthanhnien.model.Role;
+
+/**
+ * Created by kenhoang on 4/24/17.
+ */
+
+public interface User {
+    public String username = null;
+    public String Password = null;
+    public int idUser = 0;
+    public Role role = null;
+}
---
+++ app/src/main/java/app/hueic/hueicprojectdoanthanhnien/model/Role.java
@@ -0,0 +1,9 @@
+package app.hueic.hueicprojectdoanthanhnien.model;
+
+/**
+ * Created by kenhoang on 4/24/17.
+ */
+
+public class Role {
+
+}
